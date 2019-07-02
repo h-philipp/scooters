92012579d5e95aeeef164297a0e674df6875f1f0
h.philipp <h.phips96@gmail.com>
2019-07-02 09:29:09 +0200

    implemented tests for scootercontroller

--- api/controllers/scooterController.js
+++ api/controllers/scooterController.js
@@ -1,6 +1,9 @@
 
-let scooter = async function() {
-    var scooterJson = { "scooter": [ 
+let scooterJson;
+let scooterArrayName = "scooter"
+
+let loadScooterPromise = async function() {
+    let scooter = { "scooter": [ 
         {
             "name": "Scooter1"
         },
@@ -16,15 +19,36 @@ let scooter = async function() {
     };
 
     return new Promise((resolve, reject) => {
+
+        //image long database query
+        scooterJson = scooter[scooterArrayName];
+
         resolve(scooterJson);
     });
 };
 
+let scooterSizePromise = async function() {
+
+    return new Promise((resolve, reject) => {
+
+        loadScooterPromise().then(scooter => {
+            resolve(scooter.length);
+        })
+        .catch(err => {
+            reject(err);
+        })
+        
+    });
+};
+
 //exports
-exports.scooter = scooter;
+exports.scooterName = scooterArrayName;
+exports.scooter = loadScooterPromise;
+exports.scooterSize = function() { return scooterJson != undefined ? scooterJson.length : undefined; } //only available if load scooter was called before
+exports.scooterSizeAsync = scooterSizePromise;
 
 exports.getScooterResponse = async function(req, res) {
-    //get scooters
+    //get scooter
 
     await scooter().then(scooter => {
         res.status(200).send(scooter);
